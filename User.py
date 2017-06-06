1a76d17fae9c06f8254aaa75e9bd17747d111bda
Clément Escolano <clement.escolano@icloud.com>
2017-06-06 10:38:02 +0200

    Implement recommendation logic.

--- User.py
+++ User.py
@@ -1,11 +1,19 @@
+from time import time
+from math import sqrt
+
+question_delay = 5 * 60
+
+
 class User:
 
     def __init__(self, sender_id):
         self.id = sender_id
         self.good_ratings = []
         self.bad_ratings = []
+        self.neutral_ratings = []
         self.latest_movie_asked = None
         self.questions_before_recommendation = None
+        self.question_asked_time = None
 
     def answer_yes(self):
         self.good_ratings.append(self.latest_movie_asked)
@@ -17,9 +25,19 @@ class User:
         self.latest_movie_asked = None
         self.questions_before_recommendation -= 1
 
+    def answer_neutral(self):
+        self.neutral_ratings.append(self.latest_movie_asked)
+        self.latest_movie_asked = None
+
+    def set_question(self, movie_number):
+        self.latest_movie_asked = movie_number
+        if self.questions_before_recommendation is None or self.questions_before_recommendation <= 0:
+            self.questions_before_recommendation = 5
+        self.question_asked_time = int(time())
+
     def answer_question(self, message):
         clean_message = message.lower().strip()
-        if self.latest_movie_asked:
+        if self.latest_movie_asked and int(time()) - self.question_asked_time < question_delay:
             if "oui" in clean_message:
                 self.answer_yes()
                 return True
@@ -27,8 +45,25 @@ class User:
                 self.answer_no()
                 return True
             else:
-                self.latest_movie_asked = None
+                self.answer_neutral()
+                return True
+        self.latest_movie_asked = None
         return False
 
     def should_make_recommendation(self):
         return self.questions_before_recommendation <= 0
+
+    def get_norm(self):
+        norm = len(self.good_ratings) ** 2 + len(self.bad_ratings) ** 2
+        return sqrt(norm)
+
+    @staticmethod
+    def get_similarity(user_a, user_b):
+        score = 0
+        for good_rating in user_a.good_ratings:
+            if good_rating in user_b.good_ratings:
+                score += 1
+        for bad_rating in user_a.bad_ratings:
+            if bad_rating in user_b.bad_ratings:
+                score -= 1
+        return score / (user_a.get_norm() * user_b.get_norm())
--- recommendation.py
+++ recommendation.py
@@ -28,20 +28,52 @@ class Recommendation:
                     continue
                 self.ratings.append({'movie': rating[1], 'user': rating[0], 'score': rating[2]})
 
+        self.test_users = {}
         self.users = {}
+        self.process_ratings_to_users()
+
+    def process_ratings_to_users(self):
+        for rating in self.ratings:
+            if rating.id not in self.test_users.keys():
+                self.test_users[rating.id] = User(rating.id)
+            user = self.test_users[rating.id]
+            if rating.score >= 4:
+                user.good_ratings.append(rating.movie)
+            elif rating.score <= 2:
+                user.bad_ratings.append(rating.movie)
+            else:
+                user.neutral_ratings.append(rating.movie)
 
     def register_user(self, sender):
         if sender not in self.users.keys():
             self.users[sender] = User(sender)
         return self.users[sender]
 
-    @staticmethod
-    def make_recommendation(user):
-        return "Je n'ai pas de recommendation à vous faire. " + str(user.id)
+    def make_recommendation(self, user):
+        similarities = self.compute_all_similarities(user)
+        similarities.sort()
+        similarities.reverse()
+        best_match = similarities[0][0]
+        recommendations = self.get_movies_from_user(best_match)
+        return "Vos recommandations : " + ",".join(recommendations)
 
     def ask_question(self, user):
         movie_number = self.movies_list[randint(0, len(self.movies_list))]
-        user.latest_movie_asked = movie_number
-        if user.questions_before_recommendation is None or user.questions_before_recommendation <= 0:
-            user.questions_before_recommendation = 5
+        user.set_question(movie_number)
         return "Avez-vous aimé : " + self.movies[movie_number]
+
+    def compute_all_similarities(self, user):
+        similarities = []
+        for other_user in self.test_users:
+            similarities.append((User.get_similarity(user, other_user), other_user.id))
+        return similarities
+
+    def get_movies_from_user(self, user):
+        movies_list = []
+        good_movies = user.good_ratings
+        for movie_number in good_movies:
+            movies_list.append(self.movies[movie_number])
+        return movies_list
+
+
+
