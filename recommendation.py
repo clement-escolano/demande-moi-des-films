7d79a72c983cd39fb41e53e0d82403fe624d9c25
Clément Escolano <clement.escolano@icloud.com>
2017-06-13 19:38:31 +0200

    Use cleaned data.

--- recommendation.py
+++ recommendation.py
@@ -1,7 +1,6 @@
 import csv
 from User import User
 from random import randint
-import re
 
 
 class Recommendation:
@@ -9,7 +8,7 @@ class Recommendation:
     def __init__(self):
         self.movies_list = []
         self.movies = dict()
-        with open('./ml-latest-small/movies.csv', 'rt', encoding="utf-8") as moviesFile:
+        with open('./ml-latest-small/movies-popular.csv', 'rt', encoding="utf-8") as moviesFile:
             movies = csv.reader(moviesFile, delimiter=',')
             is_first = True
             for movie in movies:
@@ -19,52 +18,29 @@ class Recommendation:
                 self.movies[int(movie[0])] = movie[1]
 
         self.ratings = []
-        with open('./ml-latest-small/ratings.csv', 'rt', encoding="utf-8") as ratingsFile:
+        with open('./ml-latest-small/ratings-popular.csv', 'rt', encoding="utf-8") as ratingsFile:
             ratings = csv.reader(ratingsFile, delimiter=',')
             is_first = True
             for rating in ratings:
                 if is_first:
                     is_first = False
                     continue
-                self.ratings.append({'movie': int(rating[1]), 'user': int(rating[0]), 'score': float(rating[2])})
+                self.ratings.append({'movie': int(rating[1]), 'user': int(rating[0]), 'isAppreciated': bool(rating[2])})
 
         self.test_users = {}
         self.users = {}
         self.process_ratings_to_users()
 
-    @staticmethod
-    def get_movie_year(movie):
-        year_regex = re.compile('\d{4}')
-        return int(year_regex.search(movie).group(0))
-
-    def get_popular_movies(self):
-        movies_list = []
-        for rating in self.ratings:
-            movies_list.append(rating['movie'])
-        movie_occurences = dict()
-        for movie in movies_list:
-            if movie in movie_occurences:
-                movie_occurences[movie] += 1
-            else:
-                movie_occurences[movie] = 1
-
-        return [movie for movie in movie_occurences
-                if movie_occurences[movie] > 10 and self.get_movie_year(self.movies[movie]) > 2005]
-
     def process_ratings_to_users(self):
-        popular_movies = self.get_popular_movies()
         for rating in self.ratings:
-            if rating["movie"] in popular_movies:
-                if rating['user'] not in self.test_users.keys():
-                    self.test_users[rating['user']] = User(rating['user'])
-                user = self.test_users[rating['user']]
-                if rating['score'] >= 4:
-                    user.good_ratings.append(rating['movie'])
-                elif rating['score'] <= 2:
-                    user.bad_ratings.append(rating['movie'])
-                else:
-                    user.neutral_ratings.append(rating['movie'])
-                self.movies_list.append(rating['movie'])
+            if rating['user'] not in self.test_users.keys():
+                self.test_users[rating['user']] = User(rating['user'])
+            user = self.test_users[rating['user']]
+            if rating['isAppreciated']:
+                user.good_ratings.append(rating['movie'])
+            else:
+                user.bad_ratings.append(rating['movie'])
+            self.movies_list.append(rating['movie'])
 
     def register_user(self, sender):
         if sender not in self.users.keys():
---
+++ utils.py
@@ -0,0 +1,87 @@
+import re
+import csv
+
+year_regex = re.compile('\d{4}')
+title_regex = re.compile('(.*)\(\d{4}\)')
+
+
+def get_movie_year(movie):
+    match = year_regex.search(movie)
+    if match is None:
+        return None
+    return int(match.group(0))
+
+
+def get_appreciation(score):
+    score = float(score)
+    if score >= 4:
+        return True
+    if score <= 2:
+        return False
+    return None
+
+
+def transform_movies():
+    movies_list = []
+    movies_file = open('./ml-latest-small/movies.csv', 'rt', encoding="utf-8")
+    movies = csv.reader(movies_file, delimiter=',')
+    is_first = True
+    movies_list.append(["id", "title", "year", "genre"])
+    for movie in movies:
+        if is_first:
+            is_first = False
+            continue
+        year = get_movie_year(movie[1])
+        title = title_regex.match(movie[1]).group(1).strip() if year else movie[1]
+        movies_list.append([int(movie[0]), title, year, movie[2]])
+
+    popular_movies_file = open('./ml-latest-small/movies-popular.csv', 'wt', encoding="utf-8")
+    popular_movies_writer = csv.writer(popular_movies_file, delimiter=',')
+    for row in movies_list:
+        popular_movies_writer.writerow(row)
+
+    movies_dict = dict()
+    is_first = True
+    for movie in movies_list:
+        if is_first:
+            is_first = False
+            continue
+        movies_dict[movie[0]] = movie
+
+    return movies_dict
+
+
+def transform_ratings(movies_dict):
+    ratings_list = []
+    popular_ratings = []
+    movies_occurences = dict()
+
+    ratings_file = open('./ml-latest-small/ratings.csv', 'rt', encoding="utf-8")
+    ratings = csv.reader(ratings_file, delimiter=',')
+    is_first = True
+    for rating in ratings:
+        if is_first:
+            is_first = False
+            continue
+        is_appreciated = get_appreciation(rating[2])
+        if is_appreciated is not None:
+            ratings_list.append([int(rating[0]), int(rating[1]), is_appreciated])
+    for rating in ratings_list:
+        movie = rating[1]
+        if movie in movies_occurences:
+            movies_occurences[movie] += 1
+        else:
+            movies_occurences[movie] = 1
+
+    popular_ratings.append(["user", "movie", "isAppreciated"])
+    for rating in ratings_list:
+        if movies_occurences[rating[1]] > 50 and movies_dict[rating[1]][2] > 2000:
+            popular_ratings.append(rating)
+
+    popular_ratings_file = open('./ml-latest-small/ratings-popular.csv', 'wt', encoding="utf-8")
+    popular_ratings_writer = csv.writer(popular_ratings_file, delimiter=',')
+    for row in popular_ratings:
+        popular_ratings_writer.writerow(row)
+
+movies = transform_movies()
+transform_ratings(movies)
