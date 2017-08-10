ef548d6d2aec254382f866c23f95f964f7b4fc10
Clément Escolano <clement.escolano@icloud.com>
2017-08-10 02:54:50 +0200

    Remove recommendation code.

--- app/recommendation.py
+++ app/recommendation.py
@@ -2,6 +2,8 @@
 
 # All the recommandation logic and algorithms goes here
 
+from random import choice
+
 from app.User import User
 
 
@@ -54,76 +56,30 @@ class Recommendation:
 
     # Display the recommendation for a user
     def make_recommendation(self, user):
-        # Sort users by similarity
-        similarities = self.compute_all_similarities(user)
-        similarities.sort()
-        similarities.reverse()
+        movie = choice(list(self.movies.values())).title
 
-        # Get the 5 most similar users
-        most_similar_user_ids = [user_id for similarity, user_id in similarities[0:5]]
-        most_similar_users = [self.test_users[user_id] for user_id in most_similar_user_ids]
-        # Get the 3 movies that are most common among the 5 users
-        recommendations = self.get_best_movies_from_users(most_similar_users)[0:3]
-
-        return "Vos recommandations : " + ", ".join(recommendations)
+        return "Vos recommandations : " + ", ".join([movie])
 
     # Compute the similarity between two users
     @staticmethod
     def get_similarity(user_a, user_b):
-        score = 0
-        for good_rating in user_a.good_ratings:
-            if good_rating in user_b.good_ratings:
-                score += 1
-            if good_rating in user_b.bad_ratings:
-                score -= 1
-        for bad_rating in user_a.bad_ratings:
-            if bad_rating in user_b.bad_ratings:
-                score += 1
-            if bad_rating in user_b.good_ratings:
-                score -= 1
-
-        norm_a = Recommendation.get_user_norm(user_a)
-        norm_b = Recommendation.get_user_norm(user_b)
-
-        return score / (norm_a * norm_b)
+        return 1
 
     # Compute the similarity between a user and all the users in the data set
     def compute_all_similarities(self, user):
-        similarities = []
-        for other_user in self.test_users.values():
-            similarities.append((self.get_similarity(user, other_user), other_user.id))
-        return similarities
+        return []
 
     @staticmethod
     def get_best_movies_from_users(users):
-        # Dictionary with the number of occurences of every movie
-        counted_movies = dict()
-        for user in users:
-            for movie in Recommendation.get_user_appreciated_movies(user):
-                if movie in counted_movies:
-                    counted_movies[movie] += 1
-                else:
-                    counted_movies[movie] = 1
-        # Transform the dictionary in list to sort it
-        counted_movies_list = [(counted_movies[key], key) for key in counted_movies.keys()]
-        # Sort the list
-        counted_movies_list.sort()
-        counted_movies_list.reverse()
-        # Get only the movies title
-        best_movies_list = [value for (key, value) in counted_movies_list]
-        return best_movies_list
+        return []
 
     @staticmethod
     def get_user_appreciated_movies(user):
-        movies_list = []
-        good_movies = user.good_ratings
-        for movie in good_movies:
-            movies_list.append(movie.title)
-        return movies_list
+        return []
 
     @staticmethod
     def get_user_norm(user):
-        return 1 + len(user.good_ratings) + len(user.bad_ratings)
+        return 1
 
     # Return a vector with the normalised ratings of a user
     @staticmethod
