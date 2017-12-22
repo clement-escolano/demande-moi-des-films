69c926356b0e4fdafa6010bcdbd6b43282c83ec9
Clément Escolano <clement.escolano@icloud.com>
2017-12-22 18:12:39 +0100

    Reload flask app automatically

--- server.py
+++ server.py
@@ -45,4 +45,4 @@ def static_files(filename=None):
 if __name__ == '__main__':
     bot = chatbot.Bot()
     PORT = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=PORT)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
